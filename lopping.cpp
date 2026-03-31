398682183a148713565cb00e32fc9d2256d1d3ff
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-03-31 21:03:17 +0800

    Menambahkan logika OR pada penentuan status kelulusan

--- lopping.cpp
+++ lopping.cpp
@@ -70,4 +70,12 @@ int main(){
     cout<<"Masukkan nilai Bahasa Inggris = ";
     cin>>nilB;
 
+    rerata = (nilB + nilM) / 2;
+
+    if ((rerata >= 60) || (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
 }
