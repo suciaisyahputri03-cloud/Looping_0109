d2e8d0e107358d515b8ca05269c61e0f9d6a6fe7
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-03-31 21:18:10 +0800

    Menambahkan struktur switch case pada prosedur pilihanJurusan

--- switchCase.cpp
+++ switchCase.cpp
@@ -16,3 +16,10 @@ void input(){
     cout << "Masukkan kode jurusan (1-3): ";
     cin >> kodeJurusan;
 }
+
+//prosedur untuk pengecekan kondisi
+void pilihanjurusan(){
+    switch (kodejurusan){
+        
+    }
+}
