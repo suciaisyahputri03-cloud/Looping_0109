ebd065ce90cdd690c66197ad303bc8fb63e17bc9
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-03-31 21:08:37 +0800

    Mengubah logika kelulusan menggunakan operator AND

--- lopping.cpp
+++ lopping.cpp
@@ -93,4 +93,13 @@ int main(){
 
     cout<<"Masukkan nilai Bahasa Inggris = ";
     cin>>nilB;
+
+     rerata = (nilB + nilM) / 2;
+
+    if ((rerata >= 60) && (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
 }
